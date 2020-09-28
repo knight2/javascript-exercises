45e1f53649d8a18fcecccb48fe85f23afaadc005
Knight2 <8440420+knight2@users.noreply.github.com>
2020-09-28 08:32:28 -0400

    add functionality to repeatstring

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    toReturn = '';
+    for (let i = 0; i < num; i++){
+        toReturn += str;
+    }
+    return toReturn;
 }
 
 module.exports = repeatString
