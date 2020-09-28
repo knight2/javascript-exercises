b80e7b0295139da3d17cffa8ff399c9b2f0fd5ac
Knight2 <8440420+knight2@users.noreply.github.com>
2020-09-28 08:41:44 -0400

    pass first jasmine test

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,7 +1,7 @@
 const reverseString = function(str) {
     let strArr = str.split('');
     let newStr = '';
-    for(let i = strArr.length -1; i >0; i--){
+    for(let i = strArr.length -1; i >=0; i--){
         newStr += strArr[i];
     }
     //console.log(newStr);
