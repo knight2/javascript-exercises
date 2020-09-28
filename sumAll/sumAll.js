1251fd6dd8d82bb7dbc425ef8c8199bce8c579a2
Knight2 <8440420+knight2@users.noreply.github.com>
2020-09-28 09:01:08 -0400

    add functionality to sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if (typeof a != 'number' || typeof b != 'number'){
+        return 'ERROR';
+    }
+    if (a < 0 || b < 0){
+        return 'ERROR'
+    }
 
+    let smallest = 0;
+    let largest = 0;
+    if (a > b){
+         smallest = b;
+         largest = a;
+    } else if ( a == b){
+        return a + b;
+    }
+        else{
+        smallest = a;
+        largest = b;
+    }
+
+    let sum = 0;
+    for (let i = smallest; i <= largest; i++){
+        sum += i;
+    }
+    
+    return sum;
 }
 
 module.exports = sumAll
