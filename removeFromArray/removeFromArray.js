b6b64c852364d9df2753b2f29b7da8e18dfeb3d7
Knight2 <8440420+knight2@users.noreply.github.com>
2020-09-28 08:52:47 -0400

    add functionality to removefromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArgs) {
+    let newArr = arr;
 
+    //console.log(newArr);
+    for (let i = 0; i < theArgs.length; i++){
+        let index = newArr.indexOf(theArgs[i]);
+        if (index != -1){
+            newArr.splice(index, 1);
+        }
+        //console.log(newArr);
+    }
+    //console.log(newArr);
 }
+
+//removeFromArray([1,2,3, 'hello'], 1,2,3,'hello');
 
 module.exports = removeFromArray
