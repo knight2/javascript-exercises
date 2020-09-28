83154295c2cd14efa4f10af95d82a16a6bf9c4d9
Knight2 <8440420+knight2@users.noreply.github.com>
2020-09-28 08:39:51 -0400

    add functionality to reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let strArr = str.split('');
+    let newStr = '';
+    for(let i = strArr.length -1; i >0; i--){
+        newStr += strArr[i];
+    }
+    //console.log(newStr);
 
+    return newStr;
 }
+
 
 module.exports = reverseString
