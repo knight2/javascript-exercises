b641fb434054c207090a478534db8b0b4bd07d6f
Knight2 <8440420+knight2@users.noreply.github.com>
2020-09-29 08:13:48 -0400

    temp conversions and testing

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+  let cel = (num - 32 ) * (5/9);
+  return (Math.round(cel * 10 ) /10);
 }
 
-const ctof = function() {
-
+const ctof = function(num) {
+  let f = (num * (9/5)) + 32;
+  return (Math.round(f * 10) / 10);
 }
 
+// console.log(ftoc(31));
+// console.log(ctof(0));
 module.exports = {
   ftoc,
   ctof
