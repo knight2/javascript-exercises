5d0bdaaff44839004e923d32abd0cb03524163d2
Knight2 <8440420+knight2@users.noreply.github.com>
2020-09-29 08:06:19 -0400

    leapyear functionality and testing

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 ===0);
 }
+
+console.log(leapYears(1996));
 
 module.exports = leapYears
